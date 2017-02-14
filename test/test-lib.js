bf3b8049caab5099dd3c014adbb29a60465fa08e
Su Thaw <suthaw524@gmail.com>
2017-02-14 10:12:57 +0630

    add zawgyi test

--- test/test-lib.js
+++ test/test-lib.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import {transform} from '../index';
 
-test('transform test - should return apple emoji', async t => {
+test('transform test unicode - should return apple emoji', async t => {
     try{
         const emoji = await transform('ပန်းသီး');
         t.is('🍎',emoji, 'should return apple');
@@ -10,10 +10,19 @@ test('transform test - should return apple emoji', async t => {
     }
 });
 
-test('transform test - should return orange emoji', async t => {
+test('transform test unicode - should return orange emoji', async t => {
     try{
         const emoji = await transform('လိမ္မော်သီး');
         t.is('🍊',emoji, 'should return orange')
+    }catch(err){
+        t.fail();
+    }
+});
+
+test('transform test zawgyi - should return orange emoji', async t => {
+    try{
+        const emoji = await transform('ပန္းသီး');
+        t.is('🍎',emoji, 'should return orange')
     }catch(err){
         t.fail();
     }
